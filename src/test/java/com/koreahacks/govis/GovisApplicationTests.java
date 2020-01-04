9f996af3e7fc16ef3444c4ff1b3194213caf793d
Park Sanghun <hahadragon95@gmail.com>
2020-01-04 10:26:55 +0900

    spring boot init

---
+++ src/test/java/com/koreahacks/govis/GovisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreahacks.govis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
