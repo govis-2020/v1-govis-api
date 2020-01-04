6b208262e6e4f6c9944eabe3c84693b7bd29152a
Park Sanghun <hahadragon95@gmail.com>
2020-01-04 17:00:25 +0900

    tomcat config

--- src/main/java/com/koreahacks/govis/GovisApplication.java
+++ src/main/java/com/koreahacks/govis/GovisApplication.java
@@ -2,7 +2,11 @@ package com.koreahacks.govis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@EnableSwagger2WebMvc
+@EnableJpaRepositories
 @SpringBootApplication
 public class GovisApplication {
 
---
+++ src/main/java/com/koreahacks/govis/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.koreahacks.govis;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		setRegisterErrorPageFilter(false);
+		return application.sources(GovisApplication.class);
+	}
+
+}
